33ca356ddf737ab0cc3e318e01b9c02fc3b9d656
PatStLouis <patrick.st-louis@opsecid.ca>
2025-04-04 17:52:34 +0000

    add alsoKnownAs
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- did_webvh/core/state.py
+++ did_webvh/core/state.py
@@ -350,7 +350,10 @@ class DocumentState:
         transformed_document = json.loads(
             json.dumps(document).replace(f'did:webvh:{self.scid}:', 'did:web:')
         )
-        transformed_document['alsoKnownAs'] = [self.document_id]
+        if transformed_document.get('alsoKnownAs'):
+            transformed_document['alsoKnownAs'].append(self.document_id)
+        else:
+            transformed_document['alsoKnownAs'] = [self.document_id]
         return transformed_document
 
     @property
